32d0868d8fd31a06e8efbbf9a5e265a34f7a2f57
Jorge Cisneros <fraxito@gmail.com>
2014-10-25 21:34:49 +0200

    ALMACEN _ GitIgnore vacio

--- inicio.php
+++ inicio.php
@@ -1,3 +1,12 @@
+<?php 
+function chequeaFichero ($nombre_fichero){
+    if (file_exists($nombre_fichero)) {
+        echo '<div class = "caja cajaVerde">'.$nombre_fichero.'</div>';
+    } else {
+        echo '<div class = "caja cajaRoja">'.$nombre_fichero.'</div>';
+    }
+}
+?>    
 <!DOCTYPE html>
 
 <html>
@@ -10,32 +19,42 @@
 
         <script src="js/jquery-1.11.1.min.js"></script>
         <script src="js/bootstrap3.min.js"></script>
-
+        <style>
+        .caja {
+            width:128px;
+            height:24px;
+            text-align: center;
+        }
+        .cajaRoja{background-color: #f00;}
+        .cajaVerde{background-color: #0f0;}    
+        </style>
         
     </head>
     <body>
-
-        <div class="container">
+        <?php 
+        for ($i=1; $i<5; $i++){
+            
+        
+   echo '<div class="container" id="GRUPO'.$i.'">
 
             <div class="row">
                     <div class="col-xs-2" >
-                        
-                    </div>
+                        <br><br>';
+                            chequeaFichero('GRUPO'.$i.'/PEPErtyrtyrty');
+                            echo '<br>';
+                            chequeaFichero('GRUPO'.$i.'/index.php');                                  
+               echo '</div>
                     <div class="col-xs-10">
                         
                     </div>
             </div>
-            <hr>
-            <div class="row">
-                    <div class="col-xs-2" >
-                        
-                    </div>
-                    <div class="col-xs-10">
-<!--                        Aqui poneis algo para que quede bonito-->
-                    </div>
-            </div>            
             
+           
         </div>
+        <hr>
+        ';
+        } //del for
+        ?>
     </body>
 </html>
 
