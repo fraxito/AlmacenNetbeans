e5fe0ad0d81c167db6380a34c06850cc0a333b2c
Thomas Feneberg <thomasfeneberg@172.16.0.9>
2014-10-27 17:30:01 +0100

    Otro commit

--- Grupo2/F9965.php
+++ Grupo2/F9965.php
@@ -23,17 +23,15 @@
 
 <br>
             <div class="rating large active" data-static="false" data-score="3" data-stars="5" data-role="rating" data-show-score="true" data-score-hint="Rating: " style="height: auto;"><ul><li class="rated"></li><li class="rated"></li><li class="rated"></li><li></li><li></li></ul><span class="score-hint">Rating: 3</span></div>
-<!--            <div id="rating_1" class="fg-green rating" data-role="rating" data-show-score="true" style="height: auto;"><ul><li></li><li></li><li></li><li></li><li></li></ul><span class="score-hint">Current score: 0</span></div>
-            <div class="rating small fg-red active" data-static="false" data-score="3" data-stars="5" data-role="rating" data-show-score="true" data-score-hint="Value: " style="height: auto;"><ul><li class="rated"></li><li class="rated"></li><li class="rated"></li><li></li><li></li></ul><span class="score-hint">Value: 3</span></div>-->
-        
+
         <div class="row-fluid">
             <div class="span2" style=" margin-left: 40% " >
                     <br>
                     <br>
-                    <button id="usuarios" class="btn btn-success" type="button">Calificar</button>
+                    <button id="calificar" class="btn btn-success" type="button">Calificar</button>
                     <br>
                     <br>
-                    <button id="productos" class="btn btn-warning" type="button">Reset</button>
+                    <button id="reset" class="btn btn-warning" type="button">Reset</button>
                 </div>
                 <div id="marcoCentral" class="span10">
                     
