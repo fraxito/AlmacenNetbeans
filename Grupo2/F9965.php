68852e8bef52dc0916c0a11cd2603fb59acff6f4
Thomas Feneberg <thomasfeneberg@172.16.0.9>
2014-10-27 17:38:26 +0100

    Otro commit ... 2

--- Grupo2/F9965.php
+++ Grupo2/F9965.php
@@ -5,9 +5,10 @@
 <br>
 
 <br>
-        <div class="fotos">
-            
-            <div class="tile double" style="width:400px;height:200px; margin-left: 20% ">
+<div class="fotos" style="color: blue; background-color: lightblue">
+ 
+            <div class="tile double" style="width:400px;height:200px; margin-left: 20%;margin-bottom: 5%;margin-top: 5%">
+                
                 <div class="tile-content image-set" >
                     <img src="http://www.funnycatpix.com/_pics/Got_Stuck_So_I_Went_To_Sleep.jpg">
                     <img src="http://metroui.org.ua/images/3.jpg">
@@ -16,7 +17,7 @@
                     <img src="http://www.funnycatpix.com/_pics/Cat_Fort.jpg">
                 </div>
             </div>
-            
+            <br><br>
             Fotos diversas y malvadas
             
             <br>
@@ -24,8 +25,8 @@
 <br>
             <div class="rating large active" data-static="false" data-score="3" data-stars="5" data-role="rating" data-show-score="true" data-score-hint="Rating: " style="height: auto;"><ul><li class="rated"></li><li class="rated"></li><li class="rated"></li><li></li><li></li></ul><span class="score-hint">Rating: 3</span></div>
 
-        <div class="row-fluid">
-            <div class="span2" style=" margin-left: 40% " >
+            <div class="row-fluid" style="margin-bottom: 5% ">
+            <div class="span2" style=" margin-left: 40%" >
                     <br>
                     <br>
                     <button id="calificar" class="btn btn-success" type="button">Calificar</button>
@@ -33,9 +34,7 @@
                     <br>
                     <button id="reset" class="btn btn-warning" type="button">Reset</button>
                 </div>
-                <div id="marcoCentral" class="span10">
-                    
-                </div>
+               
             </div>
 
         
