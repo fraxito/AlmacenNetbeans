cdd4b740a7b01c1b72b81f7a9534566c8973f1de
Thomas Feneberg <thomasfeneberg@172.16.0.9>
2014-10-27 17:18:36 +0100

    Commit inicial de Thomas

---
+++ Grupo2/F9965.php
@@ -0,0 +1,49 @@
+
+
+
+
+<br>
+
+<br>
+        <div class="fotos">
+            
+            <div class="tile double" style="width:400px;height:200px; margin-left: 20% ">
+                <div class="tile-content image-set" >
+                    <img src="http://www.funnycatpix.com/_pics/Got_Stuck_So_I_Went_To_Sleep.jpg">
+                    <img src="http://metroui.org.ua/images/3.jpg">
+                    <img src="http://www.funnycatpix.com/_pics/Sleep_Together.jpg">
+                    <img src="http://www.funnycatpix.com/_pics/A_Ninja.jpg">
+                    <img src="http://www.funnycatpix.com/_pics/Cat_Fort.jpg">
+                </div>
+            </div>
+            
+            Fotos diversas y malvadas
+            
+            <br>
+
+<br>
+            <div class="rating large active" data-static="false" data-score="3" data-stars="5" data-role="rating" data-show-score="true" data-score-hint="Rating: " style="height: auto;"><ul><li class="rated"></li><li class="rated"></li><li class="rated"></li><li></li><li></li></ul><span class="score-hint">Rating: 3</span></div>
+<!--            <div id="rating_1" class="fg-green rating" data-role="rating" data-show-score="true" style="height: auto;"><ul><li></li><li></li><li></li><li></li><li></li></ul><span class="score-hint">Current score: 0</span></div>
+            <div class="rating small fg-red active" data-static="false" data-score="3" data-stars="5" data-role="rating" data-show-score="true" data-score-hint="Value: " style="height: auto;"><ul><li class="rated"></li><li class="rated"></li><li class="rated"></li><li></li><li></li></ul><span class="score-hint">Value: 3</span></div>-->
+        
+        <div class="row-fluid">
+            <div class="span2" style=" margin-left: 40% " >
+                    <br>
+                    <br>
+                    <button id="usuarios" class="btn btn-success" type="button">Calificar</button>
+                    <br>
+                    <br>
+                    <button id="productos" class="btn btn-warning" type="button">Reset</button>
+                </div>
+                <div id="marcoCentral" class="span10">
+                    
+                </div>
+            </div>
+
+        
+        </div>
+
+
+<!--            </div>-->
+        </div>
+<br>
